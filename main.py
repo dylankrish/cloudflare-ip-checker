ccfdd10a459a7ca3bea7f24f5dd5cbe344bd83fc
dylankrish <91637207+dylankrish@users.noreply.github.com>
2023-05-21 00:27:48 -0400

    Code Structure

--- main.py
+++ main.py
@@ -1,62 +1,46 @@
 import requests
+import time
 import os
 checkIPv4 = True
 checkIPv6 = False
+checkInterval = 60 # in seconds
 
-# First, we need to get the data from the API
-# We can use the requests library to do this
-# get data from cloudflare.com/ips-v4
-ipv4 = 'https://www.cloudflare.com/ips-v4'
-ipv6 = 'https://www.cloudflare.com/ips-v6'
+def checkIPs(ipType):
+    if checkIPv4:
+        # if the file does not exist, then run the code below
+        # this is for first run
+        if not os.path.exists('cloudflare-' + ipType + '.txt'):
+            response = requests.get('https://www.cloudflare.com/ips-' + ipType)
+            response.raise_for_status()  # raise exception for non-2xx status codes
+            ips = response.text.strip().split('\n')
+            # write the data to the file
+            with open('cloudflare-' + ipType + '.txt', 'w') as f:
+                f.write('\n'.join(ips))
+        # compare the data in the file with the data from the API to see if there are any changes
+        else:
+            # read the data from the file
+            with open('cloudflare-' + ipType + '.txt', 'r') as f:
+                beforeips = f.read().strip().split('\n')
+            # get the data from the API
+            response = requests.get('https://www.cloudflare.com/ips-' + ipType)
+            response.raise_for_status()
+            afterips = response.text.strip().split('\n')
+            # compare the data
+            if beforeips != afterips:
+                # find the difference between the two lists
+                diff = list(set(beforeips) - set(afterips))
+                # print that there is a difference
+                print('There is a difference in the Cloudflare ' + ipType + ' list.')
+                print('Added IPs: ' + ', '.join(diff))
+                # add the difference to the file
+                with open('cloudflare-' + ipType + '.txt', 'a') as f:
+                    f.write('\n'.join(diff))
 
-if checkIPv4:
-    # if the file does not exist, then run the code below
-    if not os.path.exists('cloudflare-ipsv4.txt'):
-        response = requests.get(ipv4)
-        response.raise_for_status()  # Raise an exception for non-2xx status codes
-        ips = response.text.strip().split('\n')
-        # write the data to a file named cloudflare-ipsv4.txt
-        with open('cloudflare-ipsv4.txt', 'w') as f:
-            f.write('\n'.join(ips))
-    # otherwise, compare the data in the file with the data from the API to see if there are any changes
-    else:
-        # read the data from the file
-        with open('cloudflare-ipsv4.txt', 'r') as f:
-            beforeips = f.read().strip().split('\n')
-        # get the data from the API
-        response = requests.get(ipv4)
-        response.raise_for_status()
-        afterips = response.text.strip().split('\n')
-        # compare the data
-        if beforeips != afterips:
-            # find the difference between the two lists
-            diff = list(set(beforeips) - set(afterips))
-            # add the difference to the file
-            with open('cloudflare-ipsv4.txt', 'a') as f:
-                f.write('\n'.join(diff))
-
-if checkIPv6:
-    # if the file does not exist, then run the code below
-    if not os.path.exists('cloudflare-ipsv6.txt'):
-        response = requests.get(ipv6)
-        response.raise_for_status()  # Raise an exception for non-2xx status codes
-        ips = response.text.strip().split('\n')
-        # write the data to a file named cloudflare-ipsv6.txt
-        with open('cloudflare-ipsv6.txt', 'w') as f:
-            f.write('\n'.join(ips))
-    # otherwise, compare the data in the file with the data from the API to see if there are any changes
-    else:
-        # read the data from the file
-        with open('cloudflare-ipsv6.txt', 'r') as f:
-            beforeips = f.read().strip().split('\n')
-        # get the data from the API
-        response = requests.get(ipv6)
-        response.raise_for_status()
-        afterips = response.text.strip().split('\n')
-        # compare the data
-        if beforeips != afterips:
-            # find the difference between the two lists
-            diff = list(set(beforeips) - set(afterips))
-            # add the difference to the file
-            with open('cloudflare-ipsv6.txt', 'a') as f:
-                f.write('\n'.join(diff))
+def main():
+    while True:
+        if checkIPv4:
+            checkIPs('ips-v4')
+        if checkIPv6:
+            checkIPs('ips-v6')
+        time.sleep(checkInterval)
+    
